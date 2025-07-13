90a093287a1e885c626951adfc21e2dd51a4bae2
Iradukunda Belyse <birad2020@gmail.com>
2025-07-13 19:39:17 +0200

    add UserProfile.jsx content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage'; // import the new component
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 
@@ -15,9 +17,15 @@ function App() {
   return (
     <>
       <div className='App'>
-        <Header/>
-        <MainContent/>
-        <Footer/>
+        <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+        />
+
+        <Header/>,
+        <MainContent/>,
+        <Footer/>,
         <WelcomeMessage/> {/* ← add this */}
         {/* other components or code... */}
       </div>
