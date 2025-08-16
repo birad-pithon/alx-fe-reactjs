5865edf698848ecd443b0599eefd4323b451b658
birad-pithon <birad2020@gmail.com>
2025-08-16 15:15:55 +0300

    add react-router-dom in app.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return(
@@ -12,7 +15,7 @@ function App() {
       </Routes>
     </Router>
   );
-  
+
   const [count, setCount] = useState(0)
 
   return (
