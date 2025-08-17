1a137ddab879d043e6f115c368ccf927faa5066d
birad-pithon <birad2020@gmail.com>
2025-08-17 18:10:28 +0300

    addi md,sm in userprofile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
