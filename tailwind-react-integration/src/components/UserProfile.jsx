aeb7face0b59c3453c00e435a3e223edea272ce8
birad-pithon <birad2020@gmail.com>
2025-08-17 18:24:35 +0300

    making responsive image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="sm:w-36 sm:h-36 md:w-40 md:h-40 rounded-full mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
         John Doe
