34bee2c2e293c8a3ad58af323d3166bb5c756952
birad-pithon <birad2020@gmail.com>
2025-08-17 18:22:19 +0300

    making responsive container adjustments

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-36 h-36 md:w-40 md:h-40 rounded-full mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="sm:w-36 sm:h-36 md:w-40 md:h-40 rounded-full mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
         John Doe
